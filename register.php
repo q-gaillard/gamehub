91645381656bbc59615a31ea4334fe0013327725
q-gaillard <quentin.gaillard@edouardgand.fr>
2026-03-23 08:57:39 +0100

    Ajout du traitement PHP de l inscription

--- register.php
+++ register.php
@@ -1,33 +1,57 @@
 <?php
 
 // création des variable
-$longueurLogin = 10;
 
 $isValid = true;
+$errorMessage = "";
 
-$login = $_POST["login"] ?? null;
-$email = $_POST["email"] ?? null;
-$password = $_POST["password"] ?? null;
-$confirm_password = $_POST["confirm_password"] ?? null;
+$login = $_POST["login"] ?? '';
+$email = $_POST["email"] ?? '';
+$password = $_POST["password"] ?? '';
+$confirm_password = $_POST["confirm_password"] ?? '';
 
 // Validation
 // es que les champs sont vide :
-if (!$login || !$email || !$password || !$confirm_password)
+if (empty($login) || empty($email) || empty($password) || empty($confirm_password))
 {
     $isValid = false;
+    $errorMessage = "tout les champs ne sont pas remplie ". $login ." ". $email ." ". $password ." ". $confirm_password;
 }
 
 // es que login est valide :
-if (!ctype_alnum($login) && strlen($login) < $longueurLogin)
+if (!preg_match("/^[a-zA-Z0-9]{4,}$/", $login))
 {
     $isValid = false;
+    $errorMessage = "le login n'est pas valide";
 }
 
 //es que l'email est valide
-if (!preg_match("#^[a-z0-9]+@[a-z0-9]{2,}\.[a-z]{2,4}$#", $email))
+if (!preg_match("/^[a-z0-9\.]+@[a-z0-9\.]{2,}\.[a-z]{2,4}$/", $email))
 {
     $isValid = false;
+    $errorMessage = "l'email n'est pas valide";
 }
 
+//es que le mot de passe est valide
+if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/", $password))
+{
+    $isValid = false;
+    $errorMessage = "le mot de passe n'est pas valide";
+}
 
+//es que le mot de passe corespond
+if (!$password == $confirm_password)
+{
+    $isValid = false;
+    $errorMessage = "votre mot de passe n'est pas identique sur les deux champs";
+}
+
+if ($isValid)
+{
+    echo "inscription réussie !";
+}
+else
+{
+    echo $errorMessage;
+}
 ?>
